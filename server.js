b9cd6f2d1de4989cb423c92fb03eb01b5ba09ff0
james <james.anthony.russell36@gmail.com>
2022-10-26 17:19:25 +1100

    send help

--- server.js
+++ server.js
@@ -2,11 +2,19 @@
 const express = require('express')
 const path = require("path")
 const fs = require('fs')
+// need to have the full path name for the readFileSync
+const notes = JSON.parse(fs.readFileSync('develop/db/db.json', 'utf8'))
+
+//creates a random generated ID for each note entry
+const NewID = () => {
+    return 'id-' + Math.random().toString(36).substring(2, 16);
+}
+
 
 //initiate the express app
 
-let app = express();
-let PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 //=============================================
 //parsing middleware for the application
@@ -21,19 +29,19 @@ app.use(express.static("./public/"))
 //=============================================
 
 // path to send file to the homepage
-app.get("/"), (req,res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html") )
-}
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html") )
+})
 
 // path is the default route for get
-app.get("*"), (req,res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-}
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-// path is to send files to the notes page.
-app.get('/notes'), (req,res) => {
+// path to send files to the notes page.
+app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
-}
+});
 
 
 //=======================================
@@ -41,7 +49,23 @@ app.get('/notes'), (req,res) => {
 //=======================================
 
 
-// 
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+            res.json(JSON.parse(data))
+        });
+});
+
+app.post('/api/notes', (req,res) => {
+    let newNote = req.body;
+    //need to make an id
+    newNote.id = NewID();
+    notes.push(newNote);
+    fs.writeFileSync("develop/db/db.json", JSON.stringify(notes));
+    res.json(notes);
+
+});
 
 
 
